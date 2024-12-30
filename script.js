07ea1585eb656cda21bab49e1da874bae3138f3f
Максим <141343669+Gustav6dbjd@users.noreply.github.com>
2024-12-30 12:39:34 +0300

    description

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-
+// прошлое дз
 document.querySelector('.nonfication').style.cssText= `
    display: flex;
     justify-content: space-between;
    background-color: red;
 `
 
-
+// дз на следующий урок
 document.querySelector('.list').addEventListener('click', function(event) {   
     if (event.target.matches('li')) {      
         console.log(event.target.textContent);
